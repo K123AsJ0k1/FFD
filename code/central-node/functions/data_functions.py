c735ff5b0e6ae700b0d3f0023a317fd05d68952c
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-02-17 18:58:52 +0200

    Central data split and context sending refactoring

--- code/central-node/functions/data_functions.py
+++ code/central-node/functions/data_functions.py
@@ -41,7 +41,7 @@ training status format:
                         - balanced-accuracy: int 
                         - accuracy: int
 '''
-# Created and works
+# Refactored and works
 def initilize_training_status():
     training_status_path = 'logs/training_status.txt'
     if os.path.exists(training_status_path):
@@ -58,7 +58,80 @@ def initilize_training_status():
     with open(training_status_path, 'w') as f:
         json.dump(training_status, f, indent=4) 
     return True
-# Works
+# refactored and works
+def store_worker_status(
+    worker_id: int,
+    worker_ip: str,
+    worker_status: str
+) -> any:
+    training_status_path = 'logs/training_status.txt'
+    
+    training_status = None
+    if not os.path.exists(training_status_path):
+        return False
+    
+    with open(training_status_path, 'r') as f:
+        training_status = json.load(f)
+
+    if worker_id == -1:
+        duplicate_id = -1
+        used_keys = []
+        
+        for worker_key in training_status['workers'].keys():
+            worker_metadata = training_status['workers'][worker_key]
+            if worker_metadata['address'] == worker_ip:
+                duplicate_id = int(worker_key)
+            used_keys.append(int(worker_key))
+            
+        set_of_used_keys = set(used_keys)
+        smallest_missing_id = 0
+        while smallest_missing_id in set_of_used_keys:
+            smallest_missing_id += 1
+        
+        local_metrics = []
+        if -1 < duplicate_id:
+            local_metrics = training_status['workers'][str(duplicate_id)]['local-metrics']
+            del training_status['workers'][str(duplicate_id)]
+
+        training_status['workers'][str(smallest_missing_id)] = {
+            'address': worker_ip,
+            'status': worker_status,
+            'local-metrics': local_metrics
+        }
+        with open(training_status_path, 'w') as f:
+            json.dump(training_status, f, indent=4)
+        
+        return smallest_missing_id, 'registered'
+    else:
+        worker_metadata = training_status['workers'][str(worker_id)]
+        if worker_metadata['address'] == worker_ip:
+            # When worker is already registered and address has stayed the same
+            training_status['workers'][str(worker_id)]['status'] = worker_status
+            with open(training_status_path, 'w') as f:
+                json.dump(training_status, f, indent=4)
+            return worker_id, 'checked'
+        else:
+            # When worker id has stayed the same, but address has changed due to load balancing
+            training_status['workers'][str(worker_id)]['status'] = worker_status
+            training_status['workers'][str(worker_id)]['address'] = worker_ip
+            with open(training_status_path, 'w') as f:
+                json.dump(training_status, f, indent=4)
+            return worker_id, 'rerouted'
+# Refactpred and works
+def store_global_metrics(
+   metrics: any
+) -> bool:
+    training_status_path = 'logs/training_status.txt'
+    if not os.path.exists(training_status_path):
+        return False
+    training_status = None
+    with open(training_status_path, 'r') as f:
+        training_status = json.load(f)
+    training_status['parameters']['global-metrics'].append(metrics)
+    with open(training_status_path, 'w') as f:
+        json.dump(training_status, f, indent=4) 
+    return True
+# Refactored and works
 def central_worker_data_split() -> bool:
     central_pool_path = 'data/central_pool.csv'
     worker_pool_path = 'data/worker_pool.csv'
@@ -82,7 +155,7 @@ def central_worker_data_split() -> bool:
     central_data_pool.to_csv(central_pool_path, index = False)    
     worker_data_pool.to_csv(worker_pool_path, index = False)
     return True
-# Works
+# Refactored and works
 def preprocess_into_train_test_and_evaluate_tensors() -> bool:
     GLOBAL_PARAMETERS = current_app.config['GLOBAL_PARAMETERS']
     CENTRAL_PARAMETERS = current_app.config['CENTRAL_PARAMETERS']
@@ -150,101 +223,105 @@ def preprocess_into_train_test_and_evaluate_tensors() -> bool:
     torch.save(eval_tensor,eval_tensor_path)
     
     return True
-# Works
+# Refactored
 def split_data_between_workers(
-    worker_amount: int
+    workers: any
 ) -> any:
     worker_pool_path = 'data/worker_pool.csv'
 
     if not os.path.exists(worker_pool_path):
-        return False
+        return None, None
     
     worker_pool_df = pd.read_csv(worker_pool_path)
-    
+
+    available_workers = []
+    for worker_key in workers.keys():
+        worker_metadata = workers[worker_key]
+        if worker_metadata['status'] == 'waiting':
+            available_workers.append(worker_key)
+
+    if len(available_workers) == 0:
+        return None, None
+        
     worker_df = worker_pool_df.sample(frac = 1)
-    worker_dfs = np.array_split(worker_df, worker_amount)
+    worker_dfs = np.array_split(worker_df, len(available_workers))
     
-    data_list = []
-    index = 1
-    for assigned_df in worker_dfs:
-        assigned_df.to_csv('data/worker_' + str(index) + '.csv', index = False)
-        data_list.append(assigned_df.values.tolist())
+    data_list = {}
+    index = 0
+    for worker_key in available_workers:
+        worker_dfs[index].to_csv('data/worker_' + worker_key + '.csv', index = False)
+        data_list[worker_key] = worker_dfs[index].values.tolist()
         index = index + 1
 
     return data_list, worker_pool_df.columns.tolist()     
-# Created
-def store_global_metrics(
-   metrics: any
-) -> bool:
-    training_status_path = 'logs/training_status.txt'
-    if not os.path.exists(training_status_path):
-        return False
-    training_status = None
-    with open(training_status_path, 'r') as f:
-        training_status = json.load(f)
-    training_status['parameters']['global-metrics'].append(metrics)
-    with open(training_status_path, 'w') as f:
-        json.dump(training_status, f, indent=4) 
-    return True
-# refactored and works
-def store_worker_status(
-    worker_id: int,
-    worker_ip: str,
-    worker_status: str
-) -> any:
-    training_status_path = 'logs/training_status.txt'
-    #print(type(worker_id))
-    training_status = None
-    if not os.path.exists(training_status_path):
-        return False
+# Refactored
+def send_context_to_workers() -> bool:
+    GLOBAL_PARAMETERS = current_app.config['GLOBAL_PARAMETERS']
     
+    training_status_path = 'logs/training_status.txt'
+    if not os.path.exists(training_status_path):
+        return False
+   
+    training_status = None
     with open(training_status_path, 'r') as f:
         training_status = json.load(f)
+   
+    global_model_path = 'models/global_model_' + str(training_status['parameters']['cycle']) + '.pth'
+    if not os.path.exists(global_model_path):
+        return False
 
-    if worker_id == -1:
-        duplicate_id = -1
-        used_keys = []
-        
-        for worker_key in training_status['workers'].keys():
-            worker_metadata = training_status['workers'][worker_key]
-            if worker_metadata['address'] == worker_ip:
-                duplicate_id = int(worker_key)
-            used_keys.append(int(worker_key))
-            
-        set_of_used_keys = set(used_keys)
-        smallest_missing_id = 0
-        while smallest_missing_id in set_of_used_keys:
-            smallest_missing_id += 1
-        
-        local_metrics = {}
-        if -1 < duplicate_id:
-            local_metrics = training_status['workers'][str(duplicate_id)]['local-metrics']
-            del training_status['workers'][str(duplicate_id)]
+    global_model = torch.load(global_model_path)
+    data_list, columns = split_data_between_workers(
+        workers = training_status['workers']
+    )
 
-        training_status['workers'][str(smallest_missing_id)] = {
-            'address': worker_ip,
-            'status': worker_status,
-            'local-metrics': local_metrics
+    if data_list == None:
+        return False
+   
+    formatted_global_model = {
+        'weights': global_model['linear.weight'].numpy().tolist(),
+        'bias': global_model['linear.bias'].numpy().tolist()
+    }
+
+    payload_status = []
+    for worker_key in training_status['workers'].keys():
+        worker_metadata = training_status['workers'][worker_key]
+        
+        if not worker_metadata['status'] == 'waiting':
+            continue
+        
+        worker_url = 'http://' + worker_metadata['address'] + ':7500/context'
+        
+        sent_worker_parameters = {
+            'id': worker_metadata['id'],
+            'address': worker_metadata['address'],
+            'status': worker_metadata['status'],
+            'columns': columns,
+            'cycle': training_status['parameters']['cycle']
         }
-        with open(training_status_path, 'w') as f:
-            json.dump(training_status, f, indent=4)
         
-        return smallest_missing_id, 'registered'
-    else:
-        worker_metadata = training_status['workers'][str(worker_id)]
-        if worker_metadata['address'] == worker_ip:
-            # When worker is already registered and address has stayed the same
-            training_status['workers'][str(worker_id)]['status'] = worker_status
-            with open(training_status_path, 'w') as f:
-                json.dump(training_status, f, indent=4)
-            return worker_id, 'checked'
-        else:
-            # When worker id has stayed the same, but address has changed due to load balancing
-            training_status['workers'][str(worker_id)]['status'] = worker_status
-            training_status['workers'][str(worker_id)]['address'] = worker_ip
-            with open(training_status_path, 'w') as f:
-                json.dump(training_status, f, indent=4)
-            return worker_id, 'rerouted'
+        payload = {
+            'global-parameters': GLOBAL_PARAMETERS,
+            'worker-parameters': sent_worker_parameters,
+            'global-model': formatted_global_model,
+            'worker-data': data_list[worker_metadata]
+        }
+
+        json_payload = json.dumps(payload) 
+
+        try:
+            response = requests.post(
+                url = worker_url, 
+                json = json_payload,
+                headers = {
+                    'Content-type':'application/json', 
+                    'Accept':'application/json'
+                }
+            )
+            payload_status.append(response.status_code)
+        except Exception as e:
+            current_app.logger.error('Context sending error' + e)
+    return True
 # Works
 def store_update(
     worker_id: str,
@@ -276,66 +353,3 @@ def store_update(
         json.dump(training_status, f, indent=4) 
 
     return True
-# Refactored
-def send_context_to_workers():
-   GLOBAL_PARAMETERS = current_app.config['GLOBAL_PARAMETERS']
-   WORKER_PARAMETERS = current_app.config['WORKER_PARAMETERS']
-   
-   training_status_path = 'logs/training_status.txt'
-   if not os.path.exists(training_status_path):
-      return False
-   
-   training_status = None
-   with open(training_status_path, 'r') as f:
-      training_status = json.load(f)
-   
-   global_model_path = 'models/global_model_' + str(training_status['parameters']['cycle']) + '.pth'
-   if not os.path.exists(global_model_path):
-      return False
-
-   global_model = torch.load(global_model_path)
-   data_list, columns = split_data_between_workers(
-      worker_amount = len(training_status['workers'])
-   )
-   
-   formatted_global_model = {
-      'weights': global_model['linear.weight'].numpy().tolist(),
-      'bias': global_model['linear.bias'].numpy().tolist()
-   }
-   
-   index = 0
-   for dict in training_status['workers']:
-      worker_address = 'http://' + dict['address'] + ':7500/context'
-      
-      worker_parameters = WORKER_PARAMETERS.copy()
-      worker_parameters['address'] = dict['address']
-      worker_parameters['worker-id'] = dict['id']
-      worker_parameters['status'] = dict['status']
-      worker_parameters['cycle'] = training_status['parameters']['cycle']
-      worker_parameters['columns'] = columns
-      
-      payload = {
-         'worker-id': dict['id'],
-         'global-parameters': GLOBAL_PARAMETERS,
-         'worker-parameters': worker_parameters,
-         'global-model': formatted_global_model,
-         'worker-data': data_list[index]
-      }
-
-      json_payload = json.dumps(payload) 
-
-      try:
-         response = requests.post(
-            url = worker_address, 
-            json = json_payload,
-            headers = {
-               'Content-type':'application/json', 
-               'Accept':'application/json'
-            }
-         )
-      except Exception as e:
-         current_app.logger.error('Context sending error')
-         current_app.logger.error(e)
-
-      index = index + 1
-
