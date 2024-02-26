4b5310a4b494221813f4a97c5d99a66b2989d723
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-02-26 18:43:51 +0200

    Getting logs

--- code/worker-node/routes/general_routes.py
+++ code/worker-node/routes/general_routes.py
@@ -9,12 +9,20 @@ general = Blueprint('general', __name__)
 @general.route('/demo', methods=["GET"]) 
 def demo():
     return 'Ok', 200
-# Created
+# Created and works
 @general.route('/logs', methods=["GET"]) 
 def worker_logs():
     with open('logs/worker.log', 'r') as f:
         logs = f.readlines()
     return render_template('logs.html', logs = logs)
+# Created
+@general.route('/worker', methods=["GET"]) 
+def worker_status():
+    worker_status_path = 'logs/worker_status.txt'
+    worker_status = None
+    with open(worker_status_path, 'r') as f:
+        worker_status = json.load(f)
+    return jsonify({'status':worker_status})
 # Refactored
 @general.route('/context', methods=["POST"]) 
 def set_training_context():
